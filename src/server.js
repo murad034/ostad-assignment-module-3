ad090e1377bc93d1094e05872e0e7c07a4897210
Karima Jaman Moon <karimajaman9876@gmail.com>
2025-09-26 12:12:39 +0600

    docker file added to developer

--- src/server.js
+++ src/server.js
@@ -12,7 +12,8 @@ const fileURLToPath = require("url");
 // console.log(process.env.PORT);
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
